318401f34eb6cdb871567f2b7a008f49722768cd
Letícia Lauriano <114084854+leticialauriano@users.noreply.github.com>
2023-08-16 16:57:18 -0300

    Adicionando projeto

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ComparacaoStrings
+{
+        internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var texto = "Este texto é um teste";
+
+            Console.WriteLine(texto.Contains("teste")); //este texto contém a palavra teste?
+            Console.WriteLine(texto.Contains("Teste")); //este texto contém a palavra teste com T?
+
+            //Ignorando o case-sensitive:
+            Console.WriteLine(texto.Contains("Teste", StringComparison.OrdinalIgnoreCase));
+
+
+            //Outra forma de comparação
+            Console.WriteLine(texto.CompareTo("Testando"));
+            Console.WriteLine(texto.CompareTo("testando"));
+            
+
+
+        }
+    }
+
+}
